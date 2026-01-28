4b0a37a735737c26afbcb219142404fe782c69be
Wallace <wallacefdealmeida@hotmail.com>
2026-01-27 21:06:22 -0300

    aplicar o timezone certo

--- src/main/java/com/treinamaisapi/TreinaMaisApiApplication.java
+++ src/main/java/com/treinamaisapi/TreinaMaisApiApplication.java
@@ -3,8 +3,16 @@ package com.treinamaisapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class TreinaMaisApiApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(TreinaMaisApiApplication.class, args);
